3e77c898e8746891d965d2b381e8b3806c908d34
treva556 <marktreva92@gmail.com>
2023-03-02 00:29:00 +0300

    done

--- run.rb
+++ run.rb
@@ -1,4 +1,4 @@
 #Review.all
 #Review.count
 #Review.new(user: user, product: product, star_rating: 5, comment: "Quenching!")
-#user = User.find_or_create_by(name: "Mark Trevor")
+#user = User.find_or_create_by(name: "Mark Trevor")#
